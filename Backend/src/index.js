6341dd6d80928c8ab94b18f92b6003fdd3af42ce
mayukh-7 <mayukhmaity7@gmail.com>
2025-10-17 17:53:10 +0530

    Deployment Completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors"
-
+import path from "path";
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 
@@ -13,7 +13,7 @@ dotenv.config();
 
 
 const PORT = process.env.PORT;
-
+const __dirname = path.resolve();
 app.use(express.json({limit: '10mb'}));
 app.use(cookieParser());
 app.use(cors({
@@ -24,6 +24,14 @@ app.use(cors({
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 
 server.listen(PORT,()=>{
